d0bde545b957abaa31ac1aa93b82199f60c9a62c
Sarojan Shrestha <myselfsarojan@gmail.com>
2025-06-10 20:23:37 +0545

    just html and css

---
+++ weatherApp/weatherApp.js
@@ -0,0 +1,20 @@
+const weatherForm = document.querySelector(".weatherForm");
+const containeer = document.querySelector(".containeer");
+
+const apiKey  = "06ee65f67f82a99b08a0ccab0ce5eb37";
+
+function getWeatherData(key){
+
+}
+
+function displayWeatherData(){
+
+}
+
+function getWeatherEmoji(){
+
+}
+
+function displayErrorMsg(message){
+    
+}
